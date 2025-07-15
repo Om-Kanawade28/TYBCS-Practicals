18fba7d059fb57612e759c105ff449f5aee60193
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2025-07-15 22:24:24 +0530

    Add two Scanner

---
+++ Java/Basics/02_add_two_Scanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Add{
+public static void main(String arg[]){
+System.out.println("Enter the First Number:-");
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+System.out.println("Enter the Second Number:-");
+int b = sc.nextInt();  
+System.out.println("Addition = "+(a+b));
+
+}}
