2894a5bd4163adcca3a90e815bdc9918af5a837d
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2025-07-16 16:49:42 +0530

    Subtraction two

---
+++ Java/Basics/06_Subtraction_Two.java
@@ -0,0 +1,9 @@
+
+class Sub{
+public static void main(String arg[]){
+int a = Integer.parseInt(arg[0]);
+int b = Integer.parseInt(arg[1]);
+
+System.out.println("subtraction "+a+"-"+b+"="+(a-b));
+
+}}
