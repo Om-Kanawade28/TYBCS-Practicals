9d259f6a3ff31e3e823c959c08a478b543d3a21b
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2025-07-16 16:48:54 +0530

    Minimum Number

---
+++ Java/Basics/05_Minimum_Number.java
@@ -0,0 +1,10 @@
+class Minimum{
+public static void main(String arg[]){
+int num1 = Integer.parseInt(arg[0]);
+int num2 = Integer.parseInt(arg[1]);
+if(num1 < num2){
+System.out.println(num1+" is minimum number");
+}else{
+System.out.println(num2+" is minimum number");
+}
+}}
